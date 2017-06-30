53326471271ca0f65ea8ca739cc6da9de440508f
Francis Bourre <francis_bourre@me.com>
2017-06-30 07:23:54 +0200

    Test added for typedef inside a module.

---
+++ test/hex/reflect/mock/pack/MockModule.hx
@@ -0,0 +1,20 @@
+package hex.reflect.mock.pack;
+
+/**
+ * ...
+ * @author Francis Bourre
+ */
+class MockModule 
+{
+
+	public function new() 
+	{
+		
+	}
+	
+}
+
+typedef InternalTypedef =
+{
+	var p : String;
+}
